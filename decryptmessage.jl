eaa562226fbf44cee3034d4bab84288de20bf445
admiralpav <pavoskar@gmail.com>
2026-08-02 14:15:33 -0700

    akjdbnakwjhdikawhdoiawjdoi

--- decryptmessage.jl
+++ decryptmessage.jl
@@ -5,8 +5,16 @@ function decryptmessage(pw, checksum, echecksum, msg)
     catch
         return false
     end
-    c = RSAdecrypt(privkey, bytes2bigint(Vector{UInt8}(echecksum)))
-
+    c = Vector{UInt8}(RSAdecrypt(privkey, bytes2bigint(Vector{UInt8}(echecksum))))
+    if c == Vector{UInt8}(checksum)
+        decryptedmsg = RSAdecrypt(privkey, bytes2bigint(Vector{UInt8}(msg)))
+        splitted = split(decryptedmsg,',',limit=3)
+        senderID = join(splitted[1],',',splitted[2])
+        messagedata = splitted[end]
+        print(senderID, " ", messagedata)
+    else
+        print(false)
+    end
 end
 decryptmessage(ARGS[1],ARGS[2],ARGS[3],ARGS[4])
-# julia decryptmessage.jl pw checksum encryptedchecksum message -> false OR [senderID(number) message data]
+# julia decryptmessage.jl pw checksum encryptedchecksum message -> false OR [senderID(publickey,n) message data]
