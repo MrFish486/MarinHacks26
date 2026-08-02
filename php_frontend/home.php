7527b9e151bb3c433bbd3d17a90473d17dbd5df4
MrFish486 <laszlodeanpav@gmail.com>
2026-08-02 12:24:23 -0700

    More things for frontend

--- php_frontend/home.php
+++ php_frontend/home.php
@@ -1,12 +1,41 @@
 <?php
 
+
 session_start();
 
-if (!isset($_SESSION["password"])) {
-	header("Location: /logout.php");
-	exit;
-}
+include("include/verify.php");
+
+$mail = [];
+
+for ($i = 0; $i < 20; $i ++) array_push($mail, "foo");
 
 ?>
 
-Home!!
+<html>
+	<head>
+		<?php include("include/head.php"); ?>
+	</head>
+	<body>
+		<div>
+			<div class="box-ne">
+				<h1>Inbox - <?= count($mail) ?> messages</h1>
+				<hr></hr>
+				<?php if (count($mail) == 0) { ?>
+					<h2>No messages.</h2>
+				<?php } else { ?>
+					<?php for ($i = 0; $i < count($mail); $i ++) { ?>
+						<div class="message">
+							<h1>MESSAGE</h1>
+						</div>
+					<?php } ?>
+				<?php } ?>
+			</div>
+			<div class="box-ne-right">
+				<h1>Message</h1>
+			</div>
+			<div class="below-box-ne">
+				<a href="/logout.php">log out</a>
+			</div>
+		</div>
+	</body>
+</html>
--- php_frontend/include/head.php
+++ php_frontend/include/head.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" href="/css/pal.css">
 <link rel="stylesheet" href="/css/index.css">
+<link rel="favicon" href="/images/icon.png">
