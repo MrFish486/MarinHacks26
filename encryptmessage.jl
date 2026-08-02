1b0a0f131fccc8ec1b996aac007f422263ca3a19
admiralpav <pavoskar@gmail.com>
2026-08-02 16:31:27 -0700

    AMOGUS

--- encryptmessage.jl
+++ encryptmessage.jl
@@ -11,8 +11,4 @@ function encryptmessage(msg, recipient)
     print(Base64.base64encode(dg)," ",Base64.base64encode(edg), " ", join(Base64.base64encode.(message), " "))
 end
 encryptmessage(ARGS[1], ARGS[2])
-<<<<<<< Updated upstream
-# julia encryptmessage.jl "message" recipient(publickey,n) -> Base64(checksum encryptedchecksum messagebody...)
-=======
 # julia encryptmessage.jl "message" recipient(publickey,n) -> Base64.(checksum encryptedchecksum messagebody...)
->>>>>>> Stashed changes
